71d2ff3bcbf60f16515354c4cb4cfd6032c07474
SteveBoytsun <100594800+SteveBoytsun@users.noreply.github.com>
2023-10-12 11:46:09 -0300

    OnUnhandledReducerError (#31)
    Co-authored-by: Steve <steve@codefics.com>
    Co-authored-by: Boppy <no-reply@boppygames.gg>

--- src/SpacetimeDBClient.cs
+++ src/SpacetimeDBClient.cs
@@ -79,6 +79,11 @@ namespace SpacetimeDB
         public event Action onSubscriptionApplied;
 
         /// <summary>
+        /// Invoked when a reducer is returned with an error and has no client-side handler.
+        /// </summary>
+        public event Action<ReducerEvent> onUnhandledReducerError;
+
+        /// <summary>
         /// Called when we receive an identity from the server
         /// </summary>
         public event Action<string, Identity, Address> onIdentityReceived;
@@ -694,10 +699,15 @@ namespace SpacetimeDB
                         case Message.TypeOneofCase.TransactionUpdate:
                             onEvent?.Invoke(message.TransactionUpdate.Event);
 
+                            bool reducerFound = false;
                             var functionName = message.TransactionUpdate.Event.FunctionCall.Reducer;
                             if (reducerEventCache.TryGetValue(functionName, out var value))
                             {
-                                value.Invoke(message.TransactionUpdate.Event);
+                                reducerFound = value.Invoke(message.TransactionUpdate.Event);
+                            }
+                            if (!reducerFound && message.TransactionUpdate.Event.Status == ClientApi.Event.Types.Status.Failed)
+                            {
+                                onUnhandledReducerError?.Invoke(message.TransactionUpdate.Event.FunctionCall.CallInfo);
                             }
 
                             break;
