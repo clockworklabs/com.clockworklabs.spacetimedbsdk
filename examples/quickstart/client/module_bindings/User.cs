clockworklabs/spacetimedb-csharp-sdk

WIP new codegen

examples/quickstart/client/module_bindings/User.cs
@@ -1,161 +1,58 @@
 // THIS FILE IS AUTOMATICALLY GENERATED BY SPACETIMEDB. EDITS TO THIS FILE
 // WILL NOT BE SAVED. MODIFY TABLES IN RUST INSTEAD.
 
+#nullable enable
+
 using System;
-using System.Collections.Generic;
 using SpacetimeDB;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
 
 namespace SpacetimeDB.Types
 {
-	[Newtonsoft.Json.JsonObject(Newtonsoft.Json.MemberSerialization.OptIn)]
-	public partial class User : IDatabaseTable
+	[SpacetimeDB.Type]
+	[DataContract]
+	public partial class User : SpacetimeDB.DatabaseTableWithPrimaryKey<User, SpacetimeDB.Types.ReducerEvent>
 	{
-		[Newtonsoft.Json.JsonProperty("identity")]
+		[DataMember(Name = "identity")]
 		public SpacetimeDB.Identity Identity;
-		[Newtonsoft.Json.JsonProperty("name")]
-		[SpacetimeDB.Some]
-		public string Name;
-		[Newtonsoft.Json.JsonProperty("online")]
+		[DataMember(Name = "name")]
+		public string? Name;
+		[DataMember(Name = "online")]
 		public bool Online;
 
-		private static Dictionary<SpacetimeDB.Identity, User> Identity_Index = new Dictionary<SpacetimeDB.Identity, User>(16);
+		protected override User GetThis() => this;
 
-		private static void InternalOnValueInserted(object insertedValue)
-		{
-			var val = (User)insertedValue;
-			Identity_Index[val.Identity] = val;
-		}
+		private static Dictionary<SpacetimeDB.Identity, User> Identity_Index = new (16);
 
-		private static void InternalOnValueDeleted(object deletedValue)
+		public override void InternalOnValueInserted()
 		{
-			var val = (User)deletedValue;
-			Identity_Index.Remove(val.Identity);
+			Identity_Index[Identity] = this;
 		}
 
-		public static SpacetimeDB.SATS.AlgebraicType GetAlgebraicType()
+		public override void InternalOnValueDeleted()
 		{
-			return SpacetimeDB.SATS.AlgebraicType.CreateProductType(new SpacetimeDB.SATS.ProductTypeElement[]
-			{
-				new SpacetimeDB.SATS.ProductTypeElement("identity", SpacetimeDB.SATS.AlgebraicType.CreateProductType(new SpacetimeDB.SATS.ProductTypeElement[]
-			{
-				new SpacetimeDB.SATS.ProductTypeElement("__identity_bytes", SpacetimeDB.SATS.AlgebraicType.CreateArrayType(SpacetimeDB.SATS.AlgebraicType.CreatePrimitiveType(SpacetimeDB.SATS.BuiltinType.Type.U8))),
-			})),
-				new SpacetimeDB.SATS.ProductTypeElement("name", SpacetimeDB.SATS.AlgebraicType.CreateSumType(new System.Collections.Generic.List<SpacetimeDB.SATS.SumTypeVariant>
-			{
-				new SpacetimeDB.SATS.SumTypeVariant("some", SpacetimeDB.SATS.AlgebraicType.CreatePrimitiveType(SpacetimeDB.SATS.BuiltinType.Type.String)),
-				new SpacetimeDB.SATS.SumTypeVariant("none", SpacetimeDB.SATS.AlgebraicType.CreateProductType(new SpacetimeDB.SATS.ProductTypeElement[]
-			{
-			})),
-			})),
-				new SpacetimeDB.SATS.ProductTypeElement("online", SpacetimeDB.SATS.AlgebraicType.CreatePrimitiveType(SpacetimeDB.SATS.BuiltinType.Type.Bool)),
-			});
+			Identity_Index.Remove(Identity);
 		}
 
-		public static explicit operator User(SpacetimeDB.SATS.AlgebraicValue value)
-		{
-			if (value == null) {
-				return null;
-			}
-			var productValue = value.AsProductValue();
-			return new User
-			{
-				Identity = SpacetimeDB.Identity.From(productValue.elements[0].AsProductValue().elements[0].AsBytes()),
-				Name = productValue.elements[1].AsSumValue().tag == 1 ? null : productValue.elements[1].AsSumValue().value.AsString(),
-				Online = productValue.elements[2].AsBool(),
-			};
-		}
-
-		public static System.Collections.Generic.IEnumerable<User> Iter()
-		{
-			foreach(var entry in SpacetimeDBClient.clientDB.GetEntries("User"))
-			{
-				yield return (User)entry.Item2;
-			}
-		}
-		public static int Count()
-		{
-			return SpacetimeDBClient.clientDB.Count("User");
-		}
-		public static User FilterByIdentity(SpacetimeDB.Identity value)
+		public static User? FilterByIdentity(SpacetimeDB.Identity value)
 		{
 			Identity_Index.TryGetValue(value, out var r);
 			return r;
 		}
 
-		public static System.Collections.Generic.IEnumerable<User> FilterByName(string? value)
+		public static IEnumerable<User> FilterByName(string? value)
 		{
-			foreach(var entry in SpacetimeDBClient.clientDB.GetEntries("User"))
-			{
-				var productValue = entry.Item1.AsProductValue();
-				var compareValue = (string?)(productValue.elements[1].AsSumValue().tag == 1 ? null : productValue.elements[1].AsSumValue().value.AsString());
-				if (compareValue == value) {
-					yield return (User)entry.Item2;
-				}
-			}
+			return Iter().Where(x => x.Name == value);
 		}
 
-		public static System.Collections.Generic.IEnumerable<User> FilterByOnline(bool value)
+		public static IEnumerable<User> FilterByOnline(bool value)
 		{
-			foreach(var entry in SpacetimeDBClient.clientDB.GetEntries("User"))
-			{
-				var productValue = entry.Item1.AsProductValue();
-				var compareValue = (bool)productValue.elements[2].AsBool();
-				if (compareValue == value) {
-					yield return (User)entry.Item2;
-				}
-			}
+			return Iter().Where(x => x.Online == value);
 		}
 
-		public static bool ComparePrimaryKey(SpacetimeDB.SATS.AlgebraicType t, SpacetimeDB.SATS.AlgebraicValue v1, SpacetimeDB.SATS.AlgebraicValue v2)
-		{
-			var primaryColumnValue1 = v1.AsProductValue().elements[0];
-			var primaryColumnValue2 = v2.AsProductValue().elements[0];
-			return SpacetimeDB.SATS.AlgebraicValue.Compare(t.product.elements[0].algebraicType, primaryColumnValue1, primaryColumnValue2);
-		}
+		public override object GetPrimaryKeyValue() => Identity;
 
-		public static SpacetimeDB.SATS.AlgebraicValue GetPrimaryKeyValue(SpacetimeDB.SATS.AlgebraicValue v)
-		{
-			return v.AsProductValue().elements[0];
-		}
-
-		public static SpacetimeDB.SATS.AlgebraicType GetPrimaryKeyType(SpacetimeDB.SATS.AlgebraicType t)
-		{
-			return t.product.elements[0].algebraicType;
-		}
-
-		public delegate void InsertEventHandler(User insertedValue, SpacetimeDB.Types.ReducerEvent dbEvent);
-		public delegate void UpdateEventHandler(User oldValue, User newValue, SpacetimeDB.Types.ReducerEvent dbEvent);
-		public delegate void DeleteEventHandler(User deletedValue, SpacetimeDB.Types.ReducerEvent dbEvent);
-		public delegate void RowUpdateEventHandler(SpacetimeDBClient.TableOp op, User oldValue, User newValue, SpacetimeDB.Types.ReducerEvent dbEvent);
-		public static event InsertEventHandler OnInsert;
-		public static event UpdateEventHandler OnUpdate;
-		public static event DeleteEventHandler OnBeforeDelete;
-		public static event DeleteEventHandler OnDelete;
-		public static event RowUpdateEventHandler OnRowUpdate;
-
-		public static void OnInsertEvent(object newValue, ClientApi.Event dbEvent)
-		{
-			OnInsert?.Invoke((User)newValue,(ReducerEvent)dbEvent?.FunctionCall.CallInfo);
-		}
-
-		public static void OnUpdateEvent(object oldValue, object newValue, ClientApi.Event dbEvent)
-		{
-			OnUpdate?.Invoke((User)oldValue,(User)newValue,(ReducerEvent)dbEvent?.FunctionCall.CallInfo);
-		}
-
-		public static void OnBeforeDeleteEvent(object oldValue, ClientApi.Event dbEvent)
-		{
-			OnBeforeDelete?.Invoke((User)oldValue,(ReducerEvent)dbEvent?.FunctionCall.CallInfo);
-		}
-
-		public static void OnDeleteEvent(object oldValue, ClientApi.Event dbEvent)
-		{
-			OnDelete?.Invoke((User)oldValue,(ReducerEvent)dbEvent?.FunctionCall.CallInfo);
-		}
-
-		public static void OnRowUpdateEvent(SpacetimeDBClient.TableOp op, object oldValue, object newValue, ClientApi.Event dbEvent)
-		{
-			OnRowUpdate?.Invoke(op, (User)oldValue,(User)newValue,(ReducerEvent)dbEvent?.FunctionCall.CallInfo);
-		}
 	}
 }

clockworklabs/com.clockworklabs.spacetimedbsdk

examples/quickstart/client/module_bindings/User.cs
@@ -1,85 +1,41 @@
 // THIS FILE IS AUTOMATICALLY GENERATED BY SPACETIMEDB. EDITS TO THIS FILE
 // WILL NOT BE SAVED. MODIFY TABLES IN RUST INSTEAD.
+// <auto-generated />
+
+#nullable enable
 
 using System;
 using SpacetimeDB;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 
 namespace SpacetimeDB.Types
 {
-	[Newtonsoft.Json.JsonObject(Newtonsoft.Json.MemberSerialization.OptIn)]
-	public partial class User : IDatabaseTable
+	[SpacetimeDB.Type]
+	[DataContract]
+	public partial class User : SpacetimeDB.DatabaseTableWithPrimaryKey<User, SpacetimeDB.Types.ReducerEvent>
 	{
-		[Newtonsoft.Json.JsonProperty("identity")]
-		public SpacetimeDB.Identity Identity;
-		[Newtonsoft.Json.JsonProperty("name")]
-		[SpacetimeDB.Some]
-		public string Name;
-		[Newtonsoft.Json.JsonProperty("online")]
+		[DataMember(Name = "identity")]
+		public SpacetimeDB.Identity Identity = new();
+		[DataMember(Name = "name")]
+		public string? Name;
+		[DataMember(Name = "online")]
 		public bool Online;
 
 		private static Dictionary<SpacetimeDB.Identity, User> Identity_Index = new(16);
 
-		private static void InternalOnValueInserted(object insertedValue)
+		public override void InternalOnValueInserted()
 		{
-			var val = (User)insertedValue;
-			Identity_Index[val.Identity] = val;
+			Identity_Index[Identity] = this;
 		}
 
-		private static void InternalOnValueDeleted(object deletedValue)
+		public override void InternalOnValueDeleted()
 		{
-			var val = (User)deletedValue;
-			Identity_Index.Remove(val.Identity);
+			Identity_Index.Remove(Identity);
 		}
 
-		public static SpacetimeDB.SATS.AlgebraicType GetAlgebraicType()
-		{
-			return SpacetimeDB.SATS.AlgebraicType.CreateProductType(new SpacetimeDB.SATS.ProductTypeElement[]
-			{
-				new SpacetimeDB.SATS.ProductTypeElement("identity", SpacetimeDB.SATS.AlgebraicType.CreateProductType(new SpacetimeDB.SATS.ProductTypeElement[]
-			{
-				new SpacetimeDB.SATS.ProductTypeElement("__identity_bytes", SpacetimeDB.SATS.AlgebraicType.CreateArrayType(SpacetimeDB.SATS.AlgebraicType.CreatePrimitiveType(SpacetimeDB.SATS.BuiltinType.Type.U8))),
-			})),
-				new SpacetimeDB.SATS.ProductTypeElement("name", SpacetimeDB.SATS.AlgebraicType.CreateSumType(new System.Collections.Generic.List<SpacetimeDB.SATS.SumTypeVariant>
-			{
-				new SpacetimeDB.SATS.SumTypeVariant("some", SpacetimeDB.SATS.AlgebraicType.CreatePrimitiveType(SpacetimeDB.SATS.BuiltinType.Type.String)),
-				new SpacetimeDB.SATS.SumTypeVariant("none", SpacetimeDB.SATS.AlgebraicType.CreateProductType(new SpacetimeDB.SATS.ProductTypeElement[]
-			{
-			})),
-			})),
-				new SpacetimeDB.SATS.ProductTypeElement("online", SpacetimeDB.SATS.AlgebraicType.CreatePrimitiveType(SpacetimeDB.SATS.BuiltinType.Type.Bool)),
-			});
-		}
-
-		public static explicit operator User(SpacetimeDB.SATS.AlgebraicValue value)
-		{
-			if (value == null) return null;
-			var productValue = value.AsProductValue();
-			return new User
-			{
-				Identity = SpacetimeDB.Identity.From(productValue.elements[0].AsProductValue().elements[0].AsBytes()),
-				Name = productValue.elements[1].AsSumValue().tag == 1 ? null : productValue.elements[1].AsSumValue().value.AsString(),
-				Online = productValue.elements[2].AsBool(),
-			};
-		}
-
-		public static IEnumerable<User> Iter()
-		{
-			return SpacetimeDBClient.clientDB.GetObjects("User").Cast<User>();
-		}
-
-		public static IEnumerable<User> Query(Func<User, bool> filter)
-		{
-			return Iter().Where(filter);
-		}
-
-		public static int Count()
-		{
-			return SpacetimeDBClient.clientDB.Count("User");
-		}
-
-		public static User FindByIdentity(SpacetimeDB.Identity value)
+		public static User? FindByIdentity(SpacetimeDB.Identity value)
 		{
 			Identity_Index.TryGetValue(value, out var r);
 			return r;
@@ -95,34 +51,7 @@ namespace SpacetimeDB.Types
 			return Query(x => x.Online == value);
 		}
 
-		private static object GetPrimaryKeyValue(object row) => ((User)row).Identity;
+		public override object GetPrimaryKeyValue() => Identity;
 
-		public delegate void InsertEventHandler(User insertedValue, SpacetimeDB.Types.ReducerEvent dbEvent);
-		public delegate void UpdateEventHandler(User oldValue, User newValue, SpacetimeDB.Types.ReducerEvent dbEvent);
-		public delegate void DeleteEventHandler(User deletedValue, SpacetimeDB.Types.ReducerEvent dbEvent);
-		public static event InsertEventHandler OnInsert;
-		public static event UpdateEventHandler OnUpdate;
-		public static event DeleteEventHandler OnBeforeDelete;
-		public static event DeleteEventHandler OnDelete;
-
-		public static void OnInsertEvent(object newValue, ClientApi.Event dbEvent)
-		{
-			OnInsert?.Invoke((User)newValue,(ReducerEvent)dbEvent?.FunctionCall.CallInfo);
-		}
-
-		public static void OnUpdateEvent(object oldValue, object newValue, ClientApi.Event dbEvent)
-		{
-			OnUpdate?.Invoke((User)oldValue,(User)newValue,(ReducerEvent)dbEvent?.FunctionCall.CallInfo);
-		}
-
-		public static void OnBeforeDeleteEvent(object oldValue, ClientApi.Event dbEvent)
-		{
-			OnBeforeDelete?.Invoke((User)oldValue,(ReducerEvent)dbEvent?.FunctionCall.CallInfo);
-		}
-
-		public static void OnDeleteEvent(object oldValue, ClientApi.Event dbEvent)
-		{
-			OnDelete?.Invoke((User)oldValue,(ReducerEvent)dbEvent?.FunctionCall.CallInfo);
-		}
 	}
 }