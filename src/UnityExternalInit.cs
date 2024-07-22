b49fc1dc102be66525325f54da1ac4240b82e979
Ingvar Stepanyan <me@rreverser.com>
2024-07-22 16:21:20 +0100

    Remove UnityExternalInit
    We have global definition for IsExternalInit now.

--- src/UnityExternalInit.cs
+++
@@ -1,7 +0,0 @@
-#if UNITY_5_3_OR_NEWER
-namespace System.Runtime.CompilerServices
-{
-    // This marker class needs to be declared for `record`s to work.
-    static class IsExternalInit {}
-}
-#endif
