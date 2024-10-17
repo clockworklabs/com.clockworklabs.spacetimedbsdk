3cab8c1a346e131857e5883fbf0b165439678913
John Detter <no-reply@boppygames.gg>
2024-10-17 02:53:27 -0500

    Disable all but one test because we don't support reconnections right now

--- unity-tests/client/Assets/PlayModeTests/PlayModeExampleTest.cs
+++ unity-tests/client/Assets/PlayModeTests/PlayModeExampleTest.cs
@@ -16,7 +16,7 @@ using Vector2 = SpacetimeDB.Types.Vector2;
 
 public class PlayModeExampleTest
 {
-    [UnityTest]
+    // [UnityTest] - This won't work until we have reconnections
     public IEnumerator SimpleConnectionTest()
     {
         PlayerPrefs.DeleteAll();
@@ -138,7 +138,7 @@ public class PlayModeExampleTest
         Debug.Assert(massEnd < massStart, "Mass should have decayed");
     }
 
-    [UnityTest]
+    // [UnityTest] - This won't work until we have reconnections
     public IEnumerator OneOffTest1()
     {
         var connected = false;
@@ -183,7 +183,7 @@ public class PlayModeExampleTest
         Debug.Log($"id: {players[0].PlayerId} Username: {players[0].Name}");
     }
 
-    [UnityTest]
+    // [UnityTest] - This won't work until we have reconnections
     public IEnumerator OneOffTest2()
     {
         var connected = false;
