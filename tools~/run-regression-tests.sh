41becfafe570941376b168f66f8c3346dd0220dc
James Gilles <jameshgilles@gmail.com>
2025-03-28 15:07:20 -0400

    Fix up regression test script

--- tools~/run-regression-tests.sh
+++ tools~/run-regression-tests.sh
@@ -8,6 +8,6 @@ STDB_PATH="$1"
 SDK_PATH="$(dirname "$0")/.."
 SDK_PATH="$(realpath "$SDK_PATH")"
 
-cargo run --manifest-path "$STDB_PATH/crates/cli/Cargo.toml" -- generate -y -l csharp -o "$SDK_PATH/examples~/regression-tests/client/module_bindings" --project-path "$SDK_PATH/examples~/regression-tests/server"
+"$SDK_PATH/tools~/gen-regression-tests.sh" "$STDB_PATH"
 cargo run --manifest-path "$STDB_PATH/crates/cli/Cargo.toml" -- publish -c -y -p "$SDK_PATH/examples~/regression-tests/server" btree-repro
 cd "$SDK_PATH/examples~/regression-tests/client" && dotnet run -c Debug
